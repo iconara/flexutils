6f311897fc085ef5a000b7e9f660a73b65f6c291
Theo <theo@iconara.net>
2009-03-08 17:42:58 +0000

    New Adt task for Rake.
    git-svn-id: svn+ssh://icebat.serveftp.com/Library/Subversion/aslib/trunk/bin/rake@193 d7d15035-0027-0410-95ab-e51e6ad04dd7

---
+++ Adt.rb
@@ -0,0 +1,46 @@
+module FlexUtils
+
+  class Adt
+    
+    attr_writer :descriptor,
+                :keyfile,
+                :keypass,
+                :name,
+                :files
+                
+    def initialize
+      @files = []
+    end
+    
+    def self.package
+      c = self.new
+      
+      yield c
+      
+      c.package!
+    end
+    
+    def package!
+      current_dir = Dir.pwd
+    
+      dir = File.dirname(@descriptor)
+      
+      Dir.chdir(dir)
+      
+      descriptor = File.basename(@descriptor)
+      name       = File.basename(@name).gsub(/\.air$/, '')
+      keyfile    = @keyfile.gsub(dir + '/', '')
+      files      = (@files || []).map { |f| f.gsub(dir + '/', '') }.join(' ')
+
+      command_string = "#{command_path 'adt'} -package -storetype pkcs12 -keystore #{keyfile} -storepass #{@keypass} #{name} #{descriptor} #{files}"
+      
+      puts command_string
+      
+      `#{command_string} 2>&1`
+      
+      Dir.chdir(current_dir)
+    end
+
+  end
+  
+end
